2746e4719b5eaad076aaff348181b96c7b3bb565
Ankit Kashyap <kashyap25ankit@gmail.com>
2024-05-24 03:15:33 +0530

    added promise in mail

--- lib/transporter.ts
+++ lib/transporter.ts
@@ -2,6 +2,9 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "sandbox.smtp.mailtrap.io",
+  tls: {
+    ciphers: "SSLv3",
+  },
   port: 2525,
   secure: false,
   auth: {
@@ -24,44 +27,35 @@ export default async function SendEmail(data: contactType) {
     text: data.text,
     html: `
     <!DOCTYPE html>
-  <html lang="en">
-  <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>${data.subject}</title>
-  </head>
-  <body>
-  <main>
-      <h1>${data.subject}</h1>  
-  
-      <br/><br/><br/>
-      
-      <h3>Hello from ${data.from} </h3>
-  
-      <p>${data.text}</p>
-  </main>
-  <script src="index.js"></script>
-  </body>
-  </html>
-  `,
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>${data.subject}</title>
+    </head>
+    <body>
+      <main>
+        <h1>${data.subject}</h1>
+        <br/><br/><br/>
+        <h3>Hello from ${data.from}</h3>
+        <p>${data.text}</p>
+      </main>
+      <script src="index.js"></script>
+    </body>
+    </html>
+    `,
   };
-
-  const res = await new Promise((res, rej) => {
+  const response = await new Promise((resolve, reject) => {
     try {
       transporter.sendMail(mailInfo);
-      res("done");
+      resolve("Email Sent");
       return 200;
     } catch (error) {
-      rej("error");
-      console.log(error);
+      reject("Error Occured");
       return 0;
     }
   });
 
-  if (res) {
-    return 200;
-  } else {
-    return 0;
-  }
+  return response;
 }
