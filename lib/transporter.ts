265ddaea09136fc4269ae5e40c8177f72e9363ac
Ankit Kashyap <kashyap25ankit@gmail.com>
2024-05-24 18:40:03 +0530

    added resend as provider

--- lib/transporter.ts
+++ lib/transporter.ts
@@ -8,31 +8,41 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default async function SendEmail(dataComing: contactType) {
-  console.log(dataComing);
-  const { data, error } = await resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
-    to: ["kashankityap@gmail.com"],
-    subject: dataComing.subject,
-    html: ` <!DOCTYPE html>
-     <html lang="en">
-     <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <title>${dataComing.subject}</title>
-     </head>
-     <body>
-       <main>
-         <h1>${dataComing.subject}</h1>
-         <h3>Hello from ${dataComing.from}</h3>
-         <p>${dataComing.text}</p>
-       </main>
-     </body>
-     </html>`,
+export default async function SendEmail(
+  dataComing: contactType
+): Promise<number> {
+  const response = await new Promise((resolve, reject) => {
+    try {
+      resend.emails.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: ["kashankityap@gmail.com"],
+        subject: dataComing.subject,
+        html: ` <!DOCTYPE html>
+       <html lang="en">
+       <head>
+         <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+         <title>${dataComing.subject}</title>
+       </head>
+       <body>
+         <main>
+           <h1>${dataComing.subject}</h1>
+           <h3>Hello from ${dataComing.from}</h3>
+           <p>${dataComing.text}</p>
+         </main>
+       </body>
+       </html>`,
+      });
+      resolve("solved");
+      return 200;
+    } catch (error) {
+      reject("not sent");
+      return 0;
+    }
   });
 
-  if (error) {
+  if (!response) {
     return 0;
   }
 
